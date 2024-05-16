60e244d789fade30a81a3869b646b8fac0ed7296
wangkeyao <1354768820@qq.com>
2024-05-16 21:35:59 +0800

    微服务模块初始化

---
+++ jxy-oj-microservice/src/main/java/com/jxy/jxyojmicroservice/JxyOjMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jxy.jxyojmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxyOjMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxyOjMicroserviceApplication.class, args);
+    }
+
+}
---
+++ jxy-oj-microservice/src/test/java/com/jxy/jxyojmicroservice/JxyOjMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxy.jxyojmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxyOjMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
